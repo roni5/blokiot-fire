96b6d4fa951dc0f07f297f69fd538db775f043e4
Roni McGuinness <roniemail1@gmail.com>
2023-01-18 15:48:45 +0000

    ready to launch

--- public/tailwind.config.js
+++ public/tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('Blokiotcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["index.html", "./node_modules/flowbite/**/*.js"],
   theme: {
